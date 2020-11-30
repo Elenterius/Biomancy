697276cdd4157f68726057bf81c02907cd4efbdc
Elenterius <elenterius@users.noreply.github.com>
2020-11-30 21:34:40 +0100

    added unique effect to khopesh items

--- src/main/java/com/github/elenterius/blightlings/handler/EquipmentHandler.java
+++ src/main/java/com/github/elenterius/blightlings/handler/EquipmentHandler.java
@@ -4,11 +4,15 @@ import com.github.elenterius.blightlings.BlightlingsMod;
 import com.github.elenterius.blightlings.init.ModEnchantments;
 import com.github.elenterius.blightlings.init.ModItems;
 import com.github.elenterius.blightlings.item.GogglesArmorItem;
+import com.github.elenterius.blightlings.item.KhopeshItem;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -19,23 +23,59 @@ import net.minecraftforge.fml.common.Mod;
 public abstract class EquipmentHandler
 {
     @SubscribeEvent
-    public static void onLivingEquipmentChange(LivingEquipmentChangeEvent event) {
+    public static void onLivingEquipmentChange(final LivingEquipmentChangeEvent event) {
         if (!event.getEntityLiving().isServerWorld()) return;
 
         if (event.getSlot() == EquipmentSlotType.HEAD) {
-            LivingEntity entity = event.getEntityLiving();
-            ItemStack oldStack = event.getFrom();
-            ItemStack newStack = event.getTo();
-
             GogglesArmorItem goggles = ModItems.TRUE_SIGHT_GOGGLES.get();
-            if (oldStack.getItem() == goggles && newStack.getItem() != goggles) { // un-equip
-                goggles.cancelEffect(entity);
+            if (event.getFrom().getItem() == goggles && event.getTo().getItem() != goggles) { // un-equip
+                goggles.cancelEffect(event.getEntityLiving());
+            }
+        }
+
+        if (event.getSlot() == EquipmentSlotType.MAINHAND) {
+            //old item
+            if (event.getFrom().getItem() instanceof KhopeshItem) {
+                ModifiableAttributeInstance modifiableAttributeInstance = event.getEntityLiving().getAttributeManager().createInstanceIfAbsent(Attributes.ATTACK_DAMAGE);
+                if (modifiableAttributeInstance != null && modifiableAttributeInstance.hasModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER)) {
+                    modifiableAttributeInstance.removeModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER);
+                }
+            }
+
+            //new item
+            if (event.getTo().getItem() instanceof KhopeshItem && event.getEntityLiving().isPassenger()) {
+                ModifiableAttributeInstance modifiableAttributeInstance = event.getEntityLiving().getAttributeManager().createInstanceIfAbsent(Attributes.ATTACK_DAMAGE);
+                if (modifiableAttributeInstance != null && !modifiableAttributeInstance.hasModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER)) {
+                    modifiableAttributeInstance.applyNonPersistentModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onEntityMount(final EntityMountEvent event) {
+        //on dismounting make sure the riding attack modifier is removed
+        if (event.isDismounting() && event.getEntityMounting() instanceof LivingEntity) {
+            ModifiableAttributeInstance modifiableAttributeInstance = ((LivingEntity) event.getEntityMounting()).getAttributeManager().createInstanceIfAbsent(Attributes.ATTACK_DAMAGE);
+            if (modifiableAttributeInstance != null && modifiableAttributeInstance.hasModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER)) {
+                modifiableAttributeInstance.removeModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER);
+            }
+        }
+
+        //on mounting add attack modifier when holding a khopesh weapon
+        if (event.isMounting() && event.getEntityMounting() instanceof LivingEntity) {
+            ItemStack stack = ((LivingEntity) event.getEntityMounting()).getHeldItemMainhand();
+            if (!stack.isEmpty() && stack.getItem() instanceof KhopeshItem) {
+                ModifiableAttributeInstance modifiableAttributeInstance = ((LivingEntity) event.getEntityMounting()).getAttributeManager().createInstanceIfAbsent(Attributes.ATTACK_DAMAGE);
+                if (modifiableAttributeInstance != null && !modifiableAttributeInstance.hasModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER)) {
+                    modifiableAttributeInstance.applyNonPersistentModifier(KhopeshItem.RIDING_ATTACK_DAMAGE_MODIFIER);
+                }
             }
         }
     }
 
     @SubscribeEvent
-    public static void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
+    public static void onLivingUpdate(final LivingEvent.LivingUpdateEvent event) {
         if (!(event.getEntityLiving() instanceof PlayerEntity)) return;
 
         if (EnchantmentHelper.getEnchantmentLevel(ModEnchantments.CLIMBING, event.getEntityLiving().getItemStackFromSlot(EquipmentSlotType.FEET)) > 0) {
@@ -44,7 +84,7 @@ public abstract class EquipmentHandler
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onLivingJump(LivingEvent.LivingJumpEvent event) {
+    public static void onLivingJump(final LivingEvent.LivingJumpEvent event) {
         if (!(event.getEntityLiving() instanceof PlayerEntity)) return;
         PlayerEntity player = (PlayerEntity) event.getEntityLiving();
 
--- src/main/java/com/github/elenterius/blightlings/item/KhopeshItem.java
+++ src/main/java/com/github/elenterius/blightlings/item/KhopeshItem.java
@@ -2,13 +2,39 @@ package com.github.elenterius.blightlings.item;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.*;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class KhopeshItem extends ModAxeItem
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+import static net.minecraft.item.ItemStack.DECIMALFORMAT;
+
+public class KhopeshItem extends AxeItem
 {
+    public static UUID RIDING_ATTACK_DAMAGE_UUID = UUID.fromString("CBD1DE77-3F1D-4E8B-839A-AA471A93D424");
+    public static AttributeModifier RIDING_ATTACK_DAMAGE_MODIFIER = new AttributeModifier(RIDING_ATTACK_DAMAGE_UUID, "riding_attack_modifier", 4f, AttributeModifier.Operation.ADDITION);
+
     public KhopeshItem(IItemTier tier, float attackDamageIn, float attackSpeedIn, Properties builder) {
         super(tier, attackDamageIn, attackSpeedIn, builder);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent(getTranslationKey(stack).replace("item", "tooltip")).setStyle(Style.EMPTY.applyFormatting(TextFormatting.GRAY)));
+        tooltip.add(new StringTextComponent(" "));
+        tooltip.add(new TranslationTextComponent("tooltip.blightlings.riding_bonus").setStyle(Style.EMPTY.applyFormatting(TextFormatting.GRAY)));
+        tooltip.add((new StringTextComponent(" ")).append(new TranslationTextComponent("attribute.modifier.plus." + RIDING_ATTACK_DAMAGE_MODIFIER.getOperation().getId(), DECIMALFORMAT.format(RIDING_ATTACK_DAMAGE_MODIFIER.getAmount()), new TranslationTextComponent("attribute.name.generic.attack_damage"))).mergeStyle(TextFormatting.BLUE));
+        super.addInformation(stack, worldIn, tooltip, flagIn);
     }
 
     @Override
