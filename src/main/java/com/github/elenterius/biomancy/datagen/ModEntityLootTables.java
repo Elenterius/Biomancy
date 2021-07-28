f60d8d9eaafa377d18bb98607ddde0854853d307
Elenterius <elenterius@users.noreply.github.com>
2021-07-28 17:39:33 +0200

    added missing loot tables

--- src/main/java/com/github/elenterius/biomancy/datagen/ModEntityLootTables.java
+++ src/main/java/com/github/elenterius/biomancy/datagen/ModEntityLootTables.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ModEntityLootTables extends EntityLootTables {
+
 	private static LootTable.Builder sheepLootTableBuilderWithDrop(IItemProvider wool) {
 		return LootTable.builder()
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(wool)))
@@ -74,6 +75,14 @@ public class ModEntityLootTables extends EntityLootTables {
 		registerLootTable(FleshBlobEntity.LOOT_TABLE_FOR_SIZE_9, fleshBlobLootTableBuilderWithDrop(8));
 		registerLootTable(FleshBlobEntity.LOOT_TABLE_FOR_SIZE_10, fleshBlobLootTableBuilderWithDrop(9));
 
+		registerLootTable(ModEntityTypes.OCULUS_OBSERVER.get(), LootTable.builder()
+				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1))
+						.addEntry(ItemLootEntry.builder(ModItems.OCULUS.get()).acceptFunction(SetCount.builder(RandomValueRange.of(0f, 1f))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f))))
+						.acceptCondition(KilledByPlayer.builder()))
+				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1))
+						.addEntry(ItemLootEntry.builder(ModItems.STOMACH.get()).acceptFunction(SetCount.builder(RandomValueRange.of(-2f, 1f))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f)))))
+		);
+
 		registerLootTable(ModEntityTypes.FAILED_SHEEP.get(), LootTable.builder()
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(ModItems.FLESH_LUMP.get()).acceptFunction(SetCount.builder(RandomValueRange.of(1, 3))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f)))))
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(ModItems.SKIN_CHUNK.get()).acceptFunction(SetCount.builder(RandomValueRange.of(2, 4))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f))))));
@@ -85,35 +94,21 @@ public class ModEntityLootTables extends EntityLootTables {
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(Items.STRING).acceptFunction(SetCount.builder(RandomValueRange.of(0, 3)))))
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(TableLootEntry.builder(ModEntityTypes.SILKY_WOOL_SHEEP.get().getLootTable()))));
 
+		registerLootTable(ModEntityTypes.FAILED_COW.get(), LootTable.builder()
+				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(ModItems.FLESH_LUMP.get()).acceptFunction(SetCount.builder(RandomValueRange.of(1, 3))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f)))))
+				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(ModItems.SKIN_CHUNK.get()).acceptFunction(SetCount.builder(RandomValueRange.of(2, 4))).acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f))))));
 		registerLootTable(ModEntityTypes.NUTRIENT_SLURRY_COW.get(), LootTable.builder()
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(Items.LEATHER).acceptFunction(SetCount.builder(RandomValueRange.of(0f, 2f)))
 						.acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f)))))
 				.addLootPool(LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(Items.BEEF).acceptFunction(SetCount.builder(RandomValueRange.of(1f, 3f)))
 						.acceptFunction(Smelt.func_215953_b().acceptCondition(EntityHasProperty.builder(LootContext.EntityTarget.THIS, ON_FIRE)))
 						.acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0f, 1f))))));
-
-//		registerLootTable(LootTables.ENTITIES_SHEEP_BLACK, sheepLootTableBuilderWithDrop(Blocks.BLACK_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_BLUE, sheepLootTableBuilderWithDrop(Blocks.BLUE_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_BROWN, sheepLootTableBuilderWithDrop(Blocks.BROWN_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_CYAN, sheepLootTableBuilderWithDrop(Blocks.CYAN_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_GRAY, sheepLootTableBuilderWithDrop(Blocks.GRAY_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_GREEN, sheepLootTableBuilderWithDrop(Blocks.GREEN_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_LIGHT_BLUE, sheepLootTableBuilderWithDrop(Blocks.LIGHT_BLUE_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_LIGHT_GRAY, sheepLootTableBuilderWithDrop(Blocks.LIGHT_GRAY_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_LIME, sheepLootTableBuilderWithDrop(Blocks.LIME_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_MAGENTA, sheepLootTableBuilderWithDrop(Blocks.MAGENTA_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_ORANGE, sheepLootTableBuilderWithDrop(Blocks.ORANGE_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_PINK, sheepLootTableBuilderWithDrop(Blocks.PINK_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_PURPLE, sheepLootTableBuilderWithDrop(Blocks.PURPLE_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_RED, sheepLootTableBuilderWithDrop(Blocks.RED_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_WHITE, sheepLootTableBuilderWithDrop(Blocks.WHITE_WOOL));
-//		registerLootTable(LootTables.ENTITIES_SHEEP_YELLOW, sheepLootTableBuilderWithDrop(Blocks.YELLOW_WOOL));
 	}
 
 	@Override
 	protected Iterable<EntityType<?>> getKnownEntities() {
 		List<EntityType<?>> entityTypes = ModEntityTypes.ENTITIES.getEntries().stream().map(RegistryObject::get).collect(Collectors.toList());
-		BiomancyMod.LOGGER.info(MarkerManager.getMarker("EntityLootTables"), String.format("generating loot tables for %s entity types...", entityTypes.size()));
+		BiomancyMod.LOGGER.info(MarkerManager.getMarker("EntityLootTables"), () -> String.format("generating loot tables for %s entity types...", entityTypes.size()));
 		return entityTypes;
 	}
 }
