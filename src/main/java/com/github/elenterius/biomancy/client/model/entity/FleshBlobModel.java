604cc90e567574fd3132e58b16c05cd7b045b7d0
Elenterius <6061273+Elenterius@users.noreply.github.com>
2022-10-18 15:27:05 +0200

    feat: update & reorganize flesh blob model and textures
    Also adds the clown easter egg

--- src/main/java/com/github/elenterius/biomancy/client/model/entity/FleshBlobModel.java
+++ src/main/java/com/github/elenterius/biomancy/client/model/entity/FleshBlobModel.java
@@ -6,24 +6,36 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
+import java.time.ZonedDateTime;
 import java.util.Locale;
 
 public class FleshBlobModel<T extends FleshBlob> extends AnimatedGeoModel<T> {
 
 	protected static final ResourceLocation[] TEXTURES = new ResourceLocation[]{
-			BiomancyMod.createRL("textures/entity/fleshkin_blob_0.png"),
-			BiomancyMod.createRL("textures/entity/fleshkin_blob_1.png"),
+			BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_neutral.png"),
+			BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_hostile.png"),
 	};
-	protected static final ResourceLocation LEGACY_TEXTURE = BiomancyMod.createRL("textures/entity/fleshkin_blob_legacy.png");
-	protected static final ResourceLocation WATCHER_TEXTURE = BiomancyMod.createRL("textures/entity/fleshkin_blob_watcher.png");
+	protected static final ResourceLocation LEGACY_TEXTURE = BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_legacy.png");
+	protected static final ResourceLocation CLOWN_TEXTURE = BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_clown.png");
+	protected static final ResourceLocation WATCHER_TEXTURE = BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_watcher.png");
+	private final boolean isClownDay;
+
+	public FleshBlobModel() {
+		ZonedDateTime now = ZonedDateTime.now();
+		isClownDay = now.getMonthValue() == 11 && now.getDayOfMonth() == 11;
+	}
 
 	@Override
 	public ResourceLocation getModelLocation(FleshBlob fleshBlob) {
-		return BiomancyMod.createRL("geo/entity/fleshkin_blob.geo.json");
+		return BiomancyMod.createRL("geo/entity/flesh_blob.geo.json");
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(FleshBlob fleshBlob) {
+		if (isClownDay) {
+			return CLOWN_TEXTURE;
+		}
+
 		if (fleshBlob.hasCustomName()) {
 			Component customName = fleshBlob.getCustomName();
 			if (customName != null) {
@@ -31,11 +43,15 @@ public class FleshBlobModel<T extends FleshBlob> extends AnimatedGeoModel<T> {
 				if (name.contains("happy")) {
 					return LEGACY_TEXTURE;
 				}
+				if (name.contains("clown")) {
+					return CLOWN_TEXTURE;
+				}
 				if (name.contains("beholder")) {
 					return WATCHER_TEXTURE;
 				}
 			}
 		}
+
 		return TEXTURES[fleshBlob.getBlobType().textureIndex];
 	}
 
