f6f7a9528116247f063b4b04957bff0f94fa20a6
Elenterius <w.elenterius@gmail.com>
2022-11-22 23:39:44 +0100

    feat: add biomancy trades to villagers

---
+++ src/main/java/com/github/elenterius/biomancy/init/ModVillagerTrades.java
@@ -0,0 +1,90 @@
+package com.github.elenterius.biomancy.init;
+
+import com.github.elenterius.biomancy.BiomancyMod;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.BasicItemListing;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = BiomancyMod.MOD_ID)
+public class ModVillagerTrades {
+
+	@SubscribeEvent
+	public static void onVillagerTrades(final VillagerTradesEvent event) {
+		if (event.getType() == VillagerProfession.BUTCHER) {
+			addButcherTrades(event.getTrades());
+		}
+		else if (event.getType() == VillagerProfession.CLERIC) {
+			addClericTrades(event.getTrades());
+		}
+	}
+
+	private static BasicItemListing buyFromPlayer(Item item, int emeralds, int maxTrades, int xp) {
+		return new BasicItemListing(new ItemStack(item), new ItemStack(Items.EMERALD, emeralds), maxTrades, xp, 0.05F);
+	}
+
+	private static BasicItemListing buyFromPlayer(Item item, int amount, int emeralds, int maxTrades, int xp) {
+		return new BasicItemListing(new ItemStack(item, amount), new ItemStack(Items.EMERALD, emeralds), maxTrades, xp, 0.05F);
+	}
+
+	private static BasicItemListing sellToPlayer(Item item, int emeralds, int maxTrades, int xp) {
+		return new BasicItemListing(emeralds, new ItemStack(item), maxTrades, xp, 0.05F);
+	}
+
+	private static void addClericTrades(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades) {
+		TradeLevel.JOURNEYMAN.addItemListings(trades,
+				buyFromPlayer(ModItems.GLASS_VIAL.get(), 4, 1, 12, 20)
+		);
+
+		TradeLevel.EXPERT.addItemListings(trades,
+				buyFromPlayer(ModItems.BILE.get(), 2, 12, 30)
+		);
+	}
+
+	private static void addButcherTrades(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades) {
+		TradeLevel.NOVICE.addItemListings(trades,
+				sellToPlayer(ModItems.MOB_FANG.get(), 12, 16, 2),
+				sellToPlayer(ModItems.MOB_CLAW.get(), 10, 16, 2)
+		);
+
+		TradeLevel.APPRENTICE.addItemListings(trades,
+				buyFromPlayer(ModItems.MOB_SINEW.get(), 4, 1, 8, 5)
+		);
+
+		TradeLevel.JOURNEYMAN.addItemListings(trades,
+				buyFromPlayer(ModItems.MOB_MARROW.get(), 4, 1, 12, 20)
+		);
+
+		TradeLevel.EXPERT.addItemListings(trades,
+				sellToPlayer(ModItems.FLESH_BITS.get(), 2, 12, 30),
+				sellToPlayer(ModItems.BONE_FRAGMENTS.get(), 2, 12, 30)
+		);
+
+		TradeLevel.MASTER.addItemListings(trades,
+				sellToPlayer(ModItems.WITHERED_MOB_MARROW.get(), 20, 8, 30)
+		);
+	}
+
+	enum TradeLevel {
+		NOVICE, APPRENTICE, JOURNEYMAN, EXPERT, MASTER;
+
+		List<VillagerTrades.ItemListing> getItemListings(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades) {
+			return trades.get(ordinal() + 1);
+		}
+
+		void addItemListings(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades, BasicItemListing... listings) {
+			getItemListings(trades).addAll(Arrays.asList(listings));
+		}
+
+	}
+
+}
