09e069a3cb360029c6e863382aa1b5ca30c3fec2
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2023-01-02 15:09:46 -0500

    Clean up recipe book categories

--- src/main/java/com/github/elenterius/biomancy/init/client/ModRecipeBookCategories.java
+++ src/main/java/com/github/elenterius/biomancy/init/client/ModRecipeBookCategories.java
@@ -1,18 +1,12 @@
 package com.github.elenterius.biomancy.init.client;
 
 import com.github.elenterius.biomancy.init.ModBioForgeTabs;
-import com.github.elenterius.biomancy.init.ModRecipeBookTypes;
-import com.github.elenterius.biomancy.init.ModRecipes;
 import com.github.elenterius.biomancy.recipe.BioForgeRecipe;
 import com.github.elenterius.biomancy.world.inventory.menu.BioForgeTab;
 import com.google.common.collect.ImmutableList;
 
 import net.minecraft.client.RecipeBookCategories;
 import net.minecraft.world.item.crafting.Recipe;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterRecipeBookCategoriesEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.HashMap;
@@ -40,25 +34,6 @@ public final class ModRecipeBookCategories {
 	};
 
 	private ModRecipeBookCategories() {}
-	
-	//TODO: Update this code for 1.19+
-	/*static void init() {
-		initBioForgeCategories();
-
-		//other, prevents warnings
-		//RecipeBookRegistry.addCategoriesFinder(ModRecipes.BIO_BREWING_RECIPE_TYPE.get(), recipe -> RecipeBookCategories.UNKNOWN);
-		//RecipeBookRegistry.addCategoriesFinder(ModRecipes.DECOMPOSING_RECIPE_TYPE.get(), recipe -> RecipeBookCategories.UNKNOWN);
-		//RecipeBookRegistry.addCategoriesFinder(ModRecipes.DIGESTING_RECIPE_TYPE.get(), recipe -> RecipeBookCategories.UNKNOWN);
-	}
-
-	private static void initBioForgeCategories() {
-		//accessing ModBioForgeTabs.REGISTRY in this context is problematic in regard to joining servers. So we have to do it manually for now...
-
-		//add stuff to the registry
-		//RecipeBookRegistry.addCategoriesToType(ModRecipeBookTypes.BIO_FORGE, List.of(searchCategory, miscCategory, blocksCategory, machinesCategory, weaponsCategory));
-		//RecipeBookRegistry.addAggregateCategories(searchCategory, List.of(miscCategory, blocksCategory, machinesCategory, weaponsCategory));
-		//RecipeBookRegistry.addCategoriesFinder(ModRecipes.BIO_FORGING_RECIPE_TYPE.get(), BIO_FORGE_BOOK_CATEGORIES_FINDER);
-	}*/
 
 	private static RecipeBookCategories createRecipeBookCategories(RegistryObject<BioForgeTab> tab) {
 		String name = tab.getId().toString().replace(":", "_");
