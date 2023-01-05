8e7da926d236823bc8d732bf8d713e9e12fd4244
Elenterius <w.elenterius@gmail.com>
2023-01-05 17:01:44 +0100

    feat: allow despoil enchantment on axes

--- src/main/java/com/github/elenterius/biomancy/init/ModEnchantments.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModEnchantments.java
@@ -6,8 +6,10 @@ import com.github.elenterius.biomancy.world.enchantment.DespoilEnchantment;
 import com.github.elenterius.biomancy.world.enchantment.MaxAmmoEnchantment;
 import com.github.elenterius.biomancy.world.enchantment.QuickShotEnchantment;
 import com.github.elenterius.biomancy.world.item.BioExtractorItem;
+import com.github.elenterius.biomancy.world.item.weapon.ClawsItem;
 import com.github.elenterius.biomancy.world.item.weapon.IGun;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,10 +20,11 @@ public final class ModEnchantments {
 
 	public static final EnchantmentCategory SYRINGE_CATEGORY = EnchantmentCategory.create("biomancy_syringe", BioExtractorItem.class::isInstance);
 	public static final EnchantmentCategory GUN_CATEGORY = EnchantmentCategory.create("biomancy_gun", IGun.class::isInstance);
+	public static final EnchantmentCategory WEAPON_CATEGORY = EnchantmentCategory.create("biomancy_weapon", item -> EnchantmentCategory.WEAPON.canEnchant(item) || item instanceof ClawsItem || item instanceof AxeItem);
 
 	public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, BiomancyMod.MOD_ID);
 
-	public static final RegistryObject<DespoilEnchantment> DESPOIL = ENCHANTMENTS.register("despoil", () -> new DespoilEnchantment(Enchantment.Rarity.RARE, EnchantmentCategory.WEAPON, EquipmentSlot.MAINHAND));
+	public static final RegistryObject<DespoilEnchantment> DESPOIL = ENCHANTMENTS.register("despoil", () -> new DespoilEnchantment(Enchantment.Rarity.RARE, WEAPON_CATEGORY, EquipmentSlot.MAINHAND));
 	public static final RegistryObject<AnestheticEnchantment> ANESTHETIC = ENCHANTMENTS.register("anesthetic", () -> new AnestheticEnchantment(Enchantment.Rarity.RARE, SYRINGE_CATEGORY, EquipmentSlot.MAINHAND));
 	public static final RegistryObject<QuickShotEnchantment> QUICK_SHOT = ENCHANTMENTS.register("quick_shot", () -> new QuickShotEnchantment(Enchantment.Rarity.VERY_RARE, EquipmentSlot.MAINHAND));
 	public static final RegistryObject<MaxAmmoEnchantment> MAX_AMMO = ENCHANTMENTS.register("max_ammo", () -> new MaxAmmoEnchantment(Enchantment.Rarity.VERY_RARE, EquipmentSlot.MAINHAND));
