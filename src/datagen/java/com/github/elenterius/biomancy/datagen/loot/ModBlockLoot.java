8c1f851e91f4f78feee461be26efbbf4538c46a7
Elenterius <w.elenterius@gmail.com>
2023-02-22 21:13:21 +0100

    feat: add tendon chain block

--- src/datagen/java/com/github/elenterius/biomancy/datagen/loot/ModBlockLoot.java
+++ src/datagen/java/com/github/elenterius/biomancy/datagen/loot/ModBlockLoot.java
@@ -107,6 +107,7 @@ public class ModBlockLoot extends BlockLoot {
 		dropSelf(ModBlocks.FLESH_FENCE_GATE.get());
 		dropSelf(ModBlocks.FLESH_LADDER.get());
 		dropSelf(ModBlocks.BIO_LANTERN.get());
+		dropSelf(ModBlocks.TENDON_CHAIN.get());
 
 		addCustom(ModBlocks.FLESH_DOOR.get(), ModBlockLoot::createFleshDoorTable);
 		addCustom(ModBlocks.FULL_FLESH_DOOR.get(), ModBlockLoot::createFleshDoorTable);
