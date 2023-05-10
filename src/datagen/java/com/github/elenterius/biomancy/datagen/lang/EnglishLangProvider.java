d796aa1ff3256ac92a1cd184b296029be806e742
Elenterius <w.elenterius@gmail.com>
2023-05-10 21:52:36 +0200

    feat: make the despoil sickle name permanent

--- src/datagen/java/com/github/elenterius/biomancy/datagen/lang/EnglishLangProvider.java
+++ src/datagen/java/com/github/elenterius/biomancy/datagen/lang/EnglishLangProvider.java
@@ -346,7 +346,7 @@ public class EnglishLangProvider extends AbstractLangProvider {
 		addItem(ModItems.VOLATILE_FLUID, "Volatile Fluid", "Combustible extract, needs further processing to be more dangerous.");
 		addItem(ModItems.BILE, "Bile", "Organic base material which is often used in bio-alchemy.");
 
-		addItem(ModItems.DESPOIL_SICKLE, "[PH] Flesh Plunderer", "A very brittle yet highly specialized tool for pillaging special loot from dying mobs.\n\nWhen held in any Hand the tool guarantees Despoil Loot to drop while receiving additional durability damage.");
+		addItem(ModItems.DESPOIL_SICKLE, "Flesh Plunderer", "A very brittle yet highly specialized tool for pillaging special loot from dying mobs.\n\nWhen held in any Hand the tool guarantees Despoil Loot to drop while receiving additional durability damage.");
 		addItem(ModItems.VIAL, "Organic Vial", "A small organic container with very high alchemical resistance, perfect for holding reactive substances.\nThe vial dissolves on use.");
 		addItem(ModItems.LIVING_FLESH, "Living Flesh", "It's alive!\nBut it looks too dumb to be the brain of a mob. You should turn it into a construct.");
 		addItem(ModItems.PRIMORDIAL_CORE, "Primordial Core", "[WIP] A ominous artifact made of flesh. It makes you feel uneasy...");
