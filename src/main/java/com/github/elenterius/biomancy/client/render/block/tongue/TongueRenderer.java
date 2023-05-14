61adc7b788599d982dd7f55b776b16cae82178c2
Elenterius <w.elenterius@gmail.com>
2023-05-14 23:01:19 +0200

    chore: refactor Tongue Block renderer

--- src/main/java/com/github/elenterius/biomancy/client/render/block/tongue/TongueRenderer.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/block/tongue/TongueRenderer.java
@@ -5,38 +5,32 @@ import com.github.elenterius.biomancy.client.render.block.CustomGeoBlockRenderer
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
 
 public class TongueRenderer extends CustomGeoBlockRenderer<TongueBlockEntity> {
 
 	private final RandomSource random = RandomSource.create();
+	private final ItemRenderer itemRenderer;
 
 	private ItemStack heldItemStack = ItemStack.EMPTY;
-	private MultiBufferSource multiBufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
-	private ResourceLocation texture;
 
-	public TongueRenderer(BlockEntityRendererProvider.Context rendererDispatcher) {
-		super(rendererDispatcher, new TongueModel());
+	public TongueRenderer(BlockEntityRendererProvider.Context context) {
+		super(context, new TongueModel());
+		itemRenderer = context.getItemRenderer();
 	}
 
 	@Override
-	public void renderEarly(TongueBlockEntity tongue, PoseStack poseStack, float ticks, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer vertexBuilder, int packedLight, int packedOverlay, float red, float green, float blue, float partialTicks) {
+	public void render(TongueBlockEntity tongue, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
 		heldItemStack = tongue.getHeldItem();
-		multiBufferSource = bufferSource != null ? bufferSource : Minecraft.getInstance().renderBuffers().bufferSource();
-		texture = getTextureLocation(tongue);
-		super.renderEarly(tongue, poseStack, ticks, bufferSource, vertexBuilder, packedLight, packedOverlay, red, green, blue, partialTicks);
+		super.render(tongue, partialTick, poseStack, bufferSource, packedLight);
 	}
 
 	@Override
@@ -45,7 +39,6 @@ public class TongueRenderer extends CustomGeoBlockRenderer<TongueBlockEntity> {
 			int itemCount = Math.min(heldItemStack.getCount(), 3);
 			int seed = Item.getId(heldItemStack.getItem()) + heldItemStack.getDamageValue();
 			renderItems(poseStack, packedLight, packedOverlay, itemCount, seed);
-			buffer = multiBufferSource.getBuffer(RenderType.entityCutout(texture));
 		}
 		super.renderRecursively(bone, poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
@@ -60,7 +53,6 @@ public class TongueRenderer extends CustomGeoBlockRenderer<TongueBlockEntity> {
 		poseStack.mulPose(Vector3f.YP.rotationDegrees(180));
 		poseStack.scale(0.75f, 0.75f, 0.75f);
 
-		ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
 		BakedModel bakedmodel = itemRenderer.getModel(heldItemStack, null, null, seed);
 		boolean isGUI3d = bakedmodel.isGui3d();
 		if (isGUI3d) poseStack.translate(0, 0, 0.1);
@@ -77,7 +69,7 @@ public class TongueRenderer extends CustomGeoBlockRenderer<TongueBlockEntity> {
 					poseStack.translate(x * 0.5f, y * 0.5f, 0);
 				}
 			}
-			itemRenderer.renderStatic(heldItemStack, ItemTransforms.TransformType.GROUND, packedLight, packedOverlay, poseStack, multiBufferSource, 0);
+			itemRenderer.renderStatic(heldItemStack, ItemTransforms.TransformType.GROUND, packedLight, packedOverlay, poseStack, rtb, 0);
 			poseStack.popPose();
 			if (!isGUI3d) poseStack.translate(0, 0, 0.025F);
 		}
