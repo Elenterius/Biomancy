ac8b646f9ad2163ba2eae6702b86ac03be6bd24f
Elenterius <w.elenterius@gmail.com>
2023-05-31 19:43:02 +0200

    chore: deprecate old Gun stuff

--- src/main/java/com/github/elenterius/biomancy/item/weapon/IGun.java
+++ src/main/java/com/github/elenterius/biomancy/item/weapon/IGun.java
@@ -12,6 +12,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
+@Deprecated
 public interface IGun {
 
 	record ProjectileProperties(float damage, float inaccuracy, int knockBack) {}
