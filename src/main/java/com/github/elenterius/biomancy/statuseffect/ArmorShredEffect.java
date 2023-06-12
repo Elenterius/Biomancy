c89700f3d44a48ac1ee3733a1475aeab77fb8575
Elenterius <w.elenterius@gmail.com>
2023-06-12 18:14:27 +0200

    chore: refactor stacking status effects

--- src/main/java/com/github/elenterius/biomancy/statuseffect/ArmorShredEffect.java
+++ src/main/java/com/github/elenterius/biomancy/statuseffect/ArmorShredEffect.java
@@ -3,19 +3,24 @@ package com.github.elenterius.biomancy.statuseffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
-public class ArmorShredEffect extends StatusEffect {
+public class ArmorShredEffect extends StatusEffect implements StackingStatusEffect {
 
-	private final int maxAttributeMultiplier;
+	private final int maxStackSize;
 
-	public ArmorShredEffect(MobEffectCategory category, int maxAttributeMultiplier, int color) {
+	public ArmorShredEffect(MobEffectCategory category, int maxStackSize, int color) {
 		super(category, color, false);
-		this.maxAttributeMultiplier = maxAttributeMultiplier;
+		this.maxStackSize = maxStackSize;
 	}
 
 	@Override
 	public double getAttributeModifierValue(int amplifier, AttributeModifier modifier) {
 		int multiplier = amplifier + 1;
-		return modifier.getAmount() * Math.min(multiplier, maxAttributeMultiplier);
+		return modifier.getAmount() * multiplier;
+	}
+
+	@Override
+	public int getMaxEffectStack() {
+		return maxStackSize;
 	}
 
 }
