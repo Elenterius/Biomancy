f58e4a9d3b7e8e9f029ce05d7f9cac5afde28c6f
Elenterius <w.elenterius@gmail.com>
2023-06-13 00:46:46 +0200

    chore(datagen): create ParticleSpriteProvider

---
+++ src/datagen/java/com/github/elenterius/biomancy/datagen/particles/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.github.elenterius.biomancy.datagen.particles;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
