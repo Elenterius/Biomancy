cba7d3e4c54904ec30669a951deeefcc5718584a
Elenterius <w.elenterius@gmail.com>
2023-06-14 18:39:09 +0200

    feat: add Blood Particles

--- src/main/java/com/github/elenterius/biomancy/init/ModParticleTypes.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModParticleTypes.java
@@ -11,6 +11,8 @@ public final class ModParticleTypes {
 
 	public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, BiomancyMod.MOD_ID);
 	public static final RegistryObject<SimpleParticleType> BLOODY_CLAWS_ATTACK = register("bloody_claws_attack", true);
+	public static final RegistryObject<SimpleParticleType> FALLING_BLOOD = register("falling_blood", false);
+	public static final RegistryObject<SimpleParticleType> LANDING_BLOOD = register("landing_blood", false);
 
 	private ModParticleTypes() {}
 
