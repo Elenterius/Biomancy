cc23f041d3ef78b1768a750051ef45963e8fa064
Elenterius <w.elenterius@gmail.com>
2023-06-15 23:30:06 +0200

    chore: deprecate ArmPoseProvider

--- src/main/java/com/github/elenterius/biomancy/item/IArmPoseProvider.java
+++ src/main/java/com/github/elenterius/biomancy/item/IArmPoseProvider.java
@@ -4,7 +4,12 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
+/**
+ * @deprecated use forges {@link IClientItemExtensions#getArmPose} instead
+ */
+@Deprecated(forRemoval = true)
 public interface IArmPoseProvider {
 
 	HumanoidModel.ArmPose getArmPose(Player player, InteractionHand usedHand, ItemStack stack);
