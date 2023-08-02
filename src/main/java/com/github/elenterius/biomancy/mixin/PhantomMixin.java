2a2449d4c4507bd1bf232be5b2ab5064fc3091ea
Elenterius <w.elenterius@gmail.com>
2023-08-03 01:12:16 +0200

    feat: add Drowsy Status Effect which prevents Phantoms from attacking

---
+++ src/main/java/com/github/elenterius/biomancy/mixin/PhantomMixin.java
@@ -0,0 +1,23 @@
+package com.github.elenterius.biomancy.mixin;
+
+import com.github.elenterius.biomancy.init.ModMobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.monster.Phantom;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class PhantomMixin {
+
+	@Inject(method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/ai/targeting/TargetingConditions;)Z", at = @At(value = "HEAD"), cancellable = true)
+	private void canAttack(LivingEntity target, TargetingConditions condition, CallbackInfoReturnable<Boolean> cir) {
+		LivingEntity attacker = (LivingEntity) (Object) this;
+		if (attacker instanceof Phantom && target.hasEffect(ModMobEffects.DROWSY.get())) {
+			cir.setReturnValue(false);
+		}
+	}
+
+}
