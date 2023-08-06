f0d2de0124ecb40a188820b9ed1434a882704d6c
Elenterius <w.elenterius@gmail.com>
2023-08-06 15:45:11 +0200

    refactor: abstract facing and waterlogging logic of the Storage Sac block

--- src/main/java/com/github/elenterius/biomancy/block/storagesac/StorageSacBlock.java
+++ src/main/java/com/github/elenterius/biomancy/block/storagesac/StorageSacBlock.java
@@ -1,5 +1,6 @@
 package com.github.elenterius.biomancy.block.storagesac;
 
+import com.github.elenterius.biomancy.block.base.WaterloggedFacingEntityBlock;
 import com.github.elenterius.biomancy.init.ModSoundEvents;
 import com.github.elenterius.biomancy.util.SoundUtil;
 import com.github.elenterius.biomancy.util.VoxelShapeUtil;
@@ -13,19 +14,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -34,10 +27,8 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
-public class StorageSacBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
+public class StorageSacBlock extends WaterloggedFacingEntityBlock {
 
-	public static final DirectionProperty FACING = BlockStateProperties.FACING;
-	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	public static final VoxelShape SHAPE_UP = createShape(Direction.UP);
 	public static final VoxelShape SHAPE_DOWN = createShape(Direction.DOWN);
 	public static final VoxelShape SHAPE_NORTH = createShape(Direction.NORTH);
@@ -47,33 +38,12 @@ public class StorageSacBlock extends BaseEntityBlock implements SimpleWaterlogge
 
 	public StorageSacBlock(Properties properties) {
 		super(properties);
-		registerDefaultState(defaultBlockState().setValue(FACING, Direction.UP).setValue(WATERLOGGED, Boolean.FALSE));
 	}
 
 	private static VoxelShape createShape(Direction direction) {
 		return Shapes.join(
 				VoxelShapeUtil.createXZRotatedTowards(direction, 4, 0, 4, 12, 2, 12),
 				VoxelShapeUtil.createXZRotatedTowards(direction, 2, 2, 2, 14, 16, 14), BooleanOp.OR);
-	}
-
-	public static Direction getFacing(BlockState state) {
-		return state.getValue(FACING);
-	}
-
-	public static boolean isWaterlogged(BlockState state) {
-		return state.getValue(WATERLOGGED);
-	}
-
-	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(FACING, WATERLOGGED);
-	}
-
-	@Nullable
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		boolean isWaterlogged = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
-		return defaultBlockState().setValue(FACING, context.getClickedFace()).setValue(WATERLOGGED, isWaterlogged);
 	}
 
 	@Nullable
@@ -117,20 +87,6 @@ public class StorageSacBlock extends BaseEntityBlock implements SimpleWaterlogge
 	}
 
 	@Override
-	public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
-		if (isWaterlogged(state)) {
-			level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
-		}
-
-		return super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
-	}
-
-	@Override
-	public FluidState getFluidState(BlockState state) {
-		return isWaterlogged(state) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-
-	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
 		return switch (getFacing(state)) {
 			case DOWN -> SHAPE_DOWN;
@@ -140,16 +96,6 @@ public class StorageSacBlock extends BaseEntityBlock implements SimpleWaterlogge
 			case EAST -> SHAPE_EAST;
 			default -> SHAPE_UP;
 		};
-	}
-
-	@Override
-	public BlockState rotate(BlockState state, Rotation rotation) {
-		return state.setValue(FACING, rotation.rotate(getFacing(state)));
-	}
-
-	@Override
-	public BlockState mirror(BlockState state, Mirror mirror) {
-		return state.rotate(mirror.getRotation(getFacing(state)));
 	}
 
 	@Override
