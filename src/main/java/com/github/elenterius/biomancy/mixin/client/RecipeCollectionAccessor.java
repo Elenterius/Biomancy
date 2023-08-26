507954c4d7cf55817ebc42ccfa6cba199555b804
Elenterius <w.elenterius@gmail.com>
2023-08-26 20:46:59 +0200

    feat: give creative players the ability to see all Bio-Forge recipe regardless of if they have unlocked them

---
+++ src/main/java/com/github/elenterius/biomancy/mixin/client/RecipeCollectionAccessor.java
@@ -0,0 +1,17 @@
+package com.github.elenterius.biomancy.mixin.client;
+
+import net.minecraft.client.gui.screens.recipebook.RecipeCollection;
+import net.minecraft.world.item.crafting.Recipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Set;
+
+@Mixin(RecipeCollection.class)
+public interface RecipeCollectionAccessor {
+	@Accessor("fitsDimensions")
+	Set<Recipe<?>> getFitDimensions();
+
+	@Accessor("craftable")
+	Set<Recipe<?>> getCraftable();
+}
