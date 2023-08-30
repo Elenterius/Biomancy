924902bbaf82e183f7fba35d445527f5b5b15324
Elenterius <w.elenterius@gmail.com>
2023-08-30 14:26:45 +0200

    chore: add EnhancedIntegerProperty for ease of use with integer based BlockStates

---
+++ src/main/java/com/github/elenterius/biomancy/mixin/IntegerPropertyAccessor.java
@@ -0,0 +1,14 @@
+package com.github.elenterius.biomancy.mixin;
+
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(IntegerProperty.class)
+public interface IntegerPropertyAccessor {
+	@Accessor("min")
+	int biomancy$getMin();
+
+	@Accessor("max")
+	int biomancy$getMax();
+}
