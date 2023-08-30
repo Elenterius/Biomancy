fba519501e31778aff1b42b6146f272def698751
Elenterius <w.elenterius@gmail.com>
2023-08-30 18:58:58 +0200

    refactor: extract all PlantTypes into a separate class

---
+++ src/main/java/com/github/elenterius/biomancy/init/ModPlantTypes.java
@@ -0,0 +1,11 @@
+package com.github.elenterius.biomancy.init;
+
+import net.minecraftforge.common.PlantType;
+
+public final class ModPlantTypes {
+
+	public static final PlantType FLESH_PLANT_TYPE = PlantType.get("flesh");
+
+	private ModPlantTypes() {}
+
+}
