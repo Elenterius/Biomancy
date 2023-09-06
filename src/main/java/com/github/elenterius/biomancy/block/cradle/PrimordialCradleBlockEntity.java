faffc4d99c67e716964fbd3ee5f01e401dc6eeb5
Elenterius <w.elenterius@gmail.com>
2023-09-07 00:58:06 +0200

    fix: fix Primordial Cradle not being marked as changed when the spread charge is consumed

--- src/main/java/com/github/elenterius/biomancy/block/cradle/PrimordialCradleBlockEntity.java
+++ src/main/java/com/github/elenterius/biomancy/block/cradle/PrimordialCradleBlockEntity.java
@@ -176,6 +176,7 @@ public class PrimordialCradleBlockEntity extends SimpleSyncedBlockEntity impleme
 		if (primalSpreadCharge < amount) return false;
 
 		primalSpreadCharge -= amount;
+		setChanged();
 
 		return true;
 	}
