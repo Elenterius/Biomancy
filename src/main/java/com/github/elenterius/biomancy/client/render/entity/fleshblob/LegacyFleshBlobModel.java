b052e92e8b9cc5cb1629eb9a896bece80a6023c2
Elenterius <w.elenterius@gmail.com>
2023-09-08 21:33:29 +0200

    feat: remove name tag for legacy flesh blob texture (legacy flesh blob is now a cradle easter egg)

--- src/main/java/com/github/elenterius/biomancy/client/render/entity/fleshblob/LegacyFleshBlobModel.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/entity/fleshblob/LegacyFleshBlobModel.java
@@ -1,10 +1,13 @@
 package com.github.elenterius.biomancy.client.render.entity.fleshblob;
 
+import com.github.elenterius.biomancy.BiomancyMod;
 import com.github.elenterius.biomancy.entity.fleshblob.EaterFleshBlob;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class LegacyFleshBlobModel<T extends EaterFleshBlob> extends AnimatedGeoModel<T> {
+
+	protected static final ResourceLocation TEXTURE = BiomancyMod.createRL("textures/entity/flesh_blob/flesh_blob_legacy.png");
 
 	@Override
 	public ResourceLocation getModelResource(T fleshBlob) {
@@ -13,7 +16,7 @@ public class LegacyFleshBlobModel<T extends EaterFleshBlob> extends AnimatedGeoM
 
 	@Override
 	public ResourceLocation getTextureResource(T fleshBlob) {
-		return FleshBlobModel.LEGACY_TEXTURE;
+		return TEXTURE;
 	}
 
 	@Override
