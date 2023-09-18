ac16e3352d9aaf0db9f5ed12c3b4f914904dbcf9
Elenterius <w.elenterius@gmail.com>
2023-09-18 19:03:33 +0200

    refactor: refactor FleshSpikeBlock

--- src/main/java/com/github/elenterius/biomancy/init/ModBlockProperties.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModBlockProperties.java
@@ -4,7 +4,6 @@ import com.github.elenterius.biomancy.block.property.*;
 import com.github.elenterius.biomancy.util.EnhancedIntegerProperty;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public final class ModBlockProperties {
 	public static final BooleanProperty IS_CRAFTING = BooleanProperty.create("crafting");
@@ -19,7 +18,7 @@ public final class ModBlockProperties {
 	public static final BooleanProperty VIAL_4 = BooleanProperty.create("vial_4");
 	public static final EnumProperty<DirectedConnection> DIRECTED_CONNECTION = EnumProperty.create("connection", DirectedConnection.class);
 	public static final EnumProperty<VertexType> VERTEX_TYPE = EnumProperty.create("vertex", VertexType.class);
-	public static final IntegerProperty SPIKES = IntegerProperty.create("spikes", 1, 3);
+	public static final EnhancedIntegerProperty SPIKES = EnhancedIntegerProperty.create("spikes", 1, 3);
 
 	private ModBlockProperties() {}
 
