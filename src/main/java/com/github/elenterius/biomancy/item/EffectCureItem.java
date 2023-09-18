4e115ae86ec1808cf02f92863219937f4bec50c9
Elenterius <w.elenterius@gmail.com>
2023-09-18 23:39:56 +0200

    refactor: refactor EffectCureItem

--- src/main/java/com/github/elenterius/biomancy/item/EffectCureItem.java
+++ src/main/java/com/github/elenterius/biomancy/item/EffectCureItem.java
@@ -1,20 +1,10 @@
 package com.github.elenterius.biomancy.item;
 
-import com.github.elenterius.biomancy.client.util.ClientTextUtil;
-import com.github.elenterius.biomancy.tooltip.HrTooltipComponent;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.inventory.tooltip.TooltipComponent;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Optional;
-
-public class EffectCureItem extends Item implements CustomTooltipProvider {
+public class EffectCureItem extends SimpleItem {
 
 	public EffectCureItem(Properties properties) {
 		super(properties);
@@ -24,16 +14,6 @@ public class EffectCureItem extends Item implements CustomTooltipProvider {
 	public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
 		if (!level.isClientSide) entity.curePotionEffects(stack);
 		return entity.eat(level, stack);
-	}
-
-	@Override
-	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag isAdvanced) {
-		tooltip.add(ClientTextUtil.getItemInfoTooltip(stack.getItem()));
-	}
-
-	@Override
-	public Optional<TooltipComponent> getTooltipImage(ItemStack stack) {
-		return Optional.of(new HrTooltipComponent());
 	}
 
 }
