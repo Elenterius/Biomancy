497cf427b7352418564e4558ea48d4f1d27234c3
Elenterius <w.elenterius@gmail.com>
2023-10-15 01:57:02 +0200

    feat(tetra-compat): make Dragon Sinew a valid cradle tribute

--- src/main/java/com/github/elenterius/biomancy/integration/tetra/TetraCompat.java
+++ src/main/java/com/github/elenterius/biomancy/integration/tetra/TetraCompat.java
@@ -1,6 +1,12 @@
 package com.github.elenterius.biomancy.integration.tetra;
 
+import com.github.elenterius.biomancy.tribute.TributeImpl;
+import com.github.elenterius.biomancy.tribute.Tributes;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import se.mickelus.tetra.TetraMod;
+import se.mickelus.tetra.TetraRegistries;
+import se.mickelus.tetra.items.loot.DragonSinewItem;
 import se.mickelus.tetra.items.modular.IModularItem;
 import se.mickelus.tetra.properties.IToolProvider;
 
@@ -14,7 +20,15 @@ public final class TetraCompat {
 	}
 
 	public static void onPostSetup() {
-		//TODO: register Dragon Sinew as Primordial Cradle tribute
+		registerDragonSinewAsTribute();
+	}
+
+	private static void registerDragonSinewAsTribute() {
+		ResourceLocation dragonSinewId = new ResourceLocation(TetraMod.MOD_ID, DragonSinewItem.identifier);
+		TetraRegistries.items.getEntries().stream()
+				.filter(registryObject -> registryObject.getId().equals(dragonSinewId))
+				.findAny()
+				.ifPresent(registryObject -> Tributes.register(registryObject.get(), TributeImpl.builder().biomass(20).lifeEnergy(25).successModifier(20).hostileModifier(20).anomalyModifier(30).create()));
 	}
 
 	static final class TetraHelperImpl implements TetraHelper {
