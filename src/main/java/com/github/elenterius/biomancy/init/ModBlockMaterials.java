d6e1db953e9de7fa699686ecd48b9b6b85215fff
Elenterius <w.elenterius@gmail.com>
2023-12-31 20:07:07 +0100

    feat(acid-fluid): make acid randomly destroy flesh veins or crack stone variants

--- src/main/java/com/github/elenterius/biomancy/init/ModBlockMaterials.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModBlockMaterials.java
@@ -2,11 +2,78 @@ package com.github.elenterius.biomancy.init;
 
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.material.PushReaction;
 
 public final class ModBlockMaterials {
 
-	public static final Material FLESH_MATERIAL = new Material.Builder(MaterialColor.COLOR_PINK).build();
+	public static final Material FLESH_MATERIAL = Builder.create().build();
+	public static final Material FLESH_VEINS_MATERIAL = Builder.create().noCollider().notSolidBlocking().nonSolid().destroyOnPush().build();
 
 	private ModBlockMaterials() {}
 
+	public static class Builder {
+		private PushReaction pushReaction = PushReaction.NORMAL;
+		private boolean blocksMotion = true;
+		private boolean flammable;
+		private boolean liquid;
+		private boolean replaceable;
+		private boolean solid = true;
+		private MaterialColor color = MaterialColor.COLOR_PINK;
+		private boolean solidBlocking = true;
+
+		private Builder() {}
+
+		public static Builder create() {
+			return new Builder();
+		}
+
+		public Builder color(MaterialColor color) {
+			this.color = color;
+			return this;
+		}
+
+		public Builder liquid() {
+			liquid = true;
+			return this;
+		}
+
+		public Builder nonSolid() {
+			solid = false;
+			return this;
+		}
+
+		public Builder noCollider() {
+			blocksMotion = false;
+			return this;
+		}
+
+		public Builder notSolidBlocking() {
+			solidBlocking = false;
+			return this;
+		}
+
+		public Builder flammable() {
+			flammable = true;
+			return this;
+		}
+
+		public Builder replaceable() {
+			replaceable = true;
+			return this;
+		}
+
+		public Builder destroyOnPush() {
+			pushReaction = PushReaction.DESTROY;
+			return this;
+		}
+
+		public Builder notPushable() {
+			pushReaction = PushReaction.BLOCK;
+			return this;
+		}
+
+		public Material build() {
+			return new Material(color, liquid, solid, blocksMotion, solidBlocking, flammable, replaceable, pushReaction);
+		}
+	}
 }
