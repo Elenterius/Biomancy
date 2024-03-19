1710d5b9b89c4f181133c1ea310e9a2b5bf768bc
Elenterius <w.elenterius@gmail.com>
2024-03-19 14:05:30 +0100

    feat: make Suspicious Stew a valid source of life energy for the Primordial Cradle
    Properly handle SuspiciousStew and read out all mob effects from it
    
    Closes: #125

---
+++ src/main/java/com/github/elenterius/biomancy/mixin/SuspiciousStewItemAccessor.java
@@ -0,0 +1,17 @@
+package com.github.elenterius.biomancy.mixin;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SuspiciousStewItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.function.Consumer;
+
+@Mixin(SuspiciousStewItem.class)
+public interface SuspiciousStewItemAccessor {
+
+	@Invoker("listPotionEffects")
+	static void biomancy$ListPotionEffects(ItemStack stack, Consumer<MobEffectInstance> output) {}
+
+}
