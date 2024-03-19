460119ec753a366f7ceed2b23096bc08f35ab228
Elenterius <w.elenterius@gmail.com>
2024-03-19 14:05:30 +0100

    feat: make Suspicious Stew a valid source of life energy for the Primordial Cradle
    Properly handle SuspiciousStew and read out all mob effects from it
    
    Closes: #125

--- src/main/java/com/github/elenterius/biomancy/tribute/MobEffectTribute.java
+++ src/main/java/com/github/elenterius/biomancy/tribute/MobEffectTribute.java
@@ -1,6 +1,7 @@
 package com.github.elenterius.biomancy.tribute;
 
 import com.github.elenterius.biomancy.init.ModMobEffects;
+import com.github.elenterius.biomancy.mixin.SuspiciousStewItemAccessor;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -8,6 +9,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.SuspiciousStewItem;
 import net.minecraft.world.item.alchemy.PotionUtils;
 
 import java.util.List;
@@ -37,25 +39,31 @@ class MobEffectTribute implements Tribute {
 	static Tribute from(ItemStack stack) {
 		boolean isPotionItem = stack.getItem() instanceof PotionItem; //we don't check if the potion has no effects because it should contain an effect in 99% of cases
 
+		boolean isSuspiciousStewItem = stack.getItem() instanceof SuspiciousStewItem;
+
 		FoodProperties food = stack.getFoodProperties(null);
 		boolean isFoodItem = food != null && !food.getEffects().isEmpty(); //we check if the food has any effects because they are optional
 
-		if (!isPotionItem && !isFoodItem) return Tribute.EMPTY; //avoid creation of new empty objects
+		if (!isPotionItem && !isSuspiciousStewItem && !isFoodItem) return Tribute.EMPTY; //avoid creation of new empty objects
 
 		MobEffectTribute mobEffectTribute = new MobEffectTribute();
 
 		if (isPotionItem) {
 			List<MobEffectInstance> effectInstances = PotionUtils.getMobEffects(stack);
-			for (MobEffectInstance instance : effectInstances) {
-				mobEffectTribute.apply(instance, 1f);
+			for (MobEffectInstance effectInstance : effectInstances) {
+				mobEffectTribute.apply(effectInstance, 1f);
 			}
+		}
+
+		if (isSuspiciousStewItem) {
+			SuspiciousStewItemAccessor.biomancy$ListPotionEffects(stack, effectInstance -> mobEffectTribute.apply(effectInstance, 0.5f));
 		}
 
 		if (isFoodItem) {
 			for (Pair<MobEffectInstance, Float> pair : food.getEffects()) {
-				MobEffectInstance instance = pair.getFirst();
+				MobEffectInstance effectInstance = pair.getFirst();
 				float chance = pair.getSecond();
-				mobEffectTribute.apply(instance, chance * 0.25f);
+				mobEffectTribute.apply(effectInstance, chance * 0.25f);
 			}
 		}
 
