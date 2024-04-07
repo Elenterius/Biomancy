2cc5ab0f4335eca95f780d554c89d99973525bdf
Elenterius <w.elenterius@gmail.com>
2024-04-07 02:00:01 +0200

    feat: show nutrient fuel value of items inside bio-machine GUIs

--- src/main/java/com/github/elenterius/biomancy/util/fuel/NutrientFuelUtil.java
+++ src/main/java/com/github/elenterius/biomancy/util/fuel/NutrientFuelUtil.java
@@ -6,13 +6,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.Set;
-import java.util.function.Predicate;
 
 public final class NutrientFuelUtil {
 
 	private NutrientFuelUtil() {}
-
-	public static final byte DEFAULT_FUEL_VALUE = 1;
 
 	public static final Set<Item> FUEL_ITEMS = Set.of(
 			ModItems.NUTRIENTS.get(),
@@ -20,33 +17,28 @@ public final class NutrientFuelUtil {
 			ModItems.NUTRIENT_BAR.get()
 	);
 
-	/**
-	 * valid items for gun ammunition
-	 */
-	public static final Predicate<ItemStack> AMMO_PREDICATE = stack -> stack.getItem() == ModItems.NUTRIENT_PASTE.get();
-
 	public static boolean isValidFuel(ItemStack stack) {
 		if (stack.isEmpty()) return false;
 		if (FUEL_ITEMS.contains(stack.getItem())) return true;
-		return getFoodNutritionValue(stack) > 0;
+		return getFoodNutrition(stack) > 0;
 	}
 
 	public static int getFuelValue(ItemStack stack) {
-		Item item = stack.getItem();
+		if (stack.isEmpty()) return 0;
 
-		if (item == ModItems.NUTRIENTS.get()) return DEFAULT_FUEL_VALUE;
-		if (item == ModItems.NUTRIENT_PASTE.get()) return DEFAULT_FUEL_VALUE * 5;
-		if (item == ModItems.NUTRIENT_BAR.get()) return (DEFAULT_FUEL_VALUE * 5) * 9;
-		if (getFoodNutritionValue(stack) > 0) return DEFAULT_FUEL_VALUE * 2;
-		return 0;
+		Item item = stack.getItem();
+		if (item == ModItems.NUTRIENTS.get()) return 1;
+		if (item == ModItems.NUTRIENT_PASTE.get()) return 5;
+		if (item == ModItems.NUTRIENT_BAR.get()) return 5 * 9;
+
+		return getFoodNutrition(stack);
 	}
 
-	public static int getFoodNutritionValue(ItemStack stack) {
-		if (stack.isEdible()) {
-			FoodProperties foodProperties = stack.getItem().getFoodProperties(stack, null);
-			if (foodProperties != null) return foodProperties.getNutrition();
-		}
-		return 0;
+	public static int getFoodNutrition(ItemStack stack) {
+		if (!stack.isEdible()) return 0;
+
+		FoodProperties foodProperties = stack.getFoodProperties(null);
+		return foodProperties != null ? foodProperties.getNutrition() : 0;
 	}
 
 }
