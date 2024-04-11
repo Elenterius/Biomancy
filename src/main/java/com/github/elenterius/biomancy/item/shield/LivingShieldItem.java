d81e6fa7ea1fe3333b9a96de8dc1cf233b266186
Elenterius <w.elenterius@gmail.com>
2024-04-11 20:15:01 +0200

    feat: make The Thorn shield a living tool

---
+++ src/main/java/com/github/elenterius/biomancy/item/shield/LivingShieldItem.java
@@ -0,0 +1,162 @@
+package com.github.elenterius.biomancy.item.shield;
+
+import com.github.elenterius.biomancy.api.livingtool.SimpleLivingTool;
+import com.github.elenterius.biomancy.init.ModSoundEvents;
+import com.github.elenterius.biomancy.styles.ColorStyles;
+import com.github.elenterius.biomancy.styles.TextComponentUtil;
+import com.github.elenterius.biomancy.util.ComponentUtil;
+import com.github.elenterius.biomancy.util.MobUtil;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ToolAction;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LivingShieldItem extends SimpleShieldItem implements SimpleLivingTool {
+
+	private final int maxNutrients;
+
+	public LivingShieldItem(int maxNutrients, Properties properties) {
+		super(properties);
+		this.maxNutrients = maxNutrients;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag isAdvanced) {
+		super.appendHoverText(stack, level, tooltip, isAdvanced);
+		tooltip.add(ComponentUtil.emptyLine());
+		appendLivingToolTooltip(stack, tooltip);
+	}
+
+	@Override
+	public int getMaxNutrients(ItemStack stack) {
+		return maxNutrients;
+	}
+
+	@Override
+	public boolean overrideStackedOnOther(ItemStack stack, Slot slot, ClickAction action, Player player) {
+		if (handleOverrideStackedOnOther(stack, slot, action, player)) {
+			playSound(player, ModSoundEvents.FLESHKIN_EAT.get());
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean overrideOtherStackedOnMe(ItemStack stack, ItemStack other, Slot slot, ClickAction action, Player player, SlotAccess access) {
+		if (handleOverrideOtherStackedOnMe(stack, other, slot, action, player, access)) {
+			playSound(player, ModSoundEvents.FLESHKIN_EAT.get());
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+		return super.canPerformAction(stack, toolAction) && hasNutrients(stack);
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+		ItemStack stack = player.getItemInHand(hand);
+
+		if (!hasNutrients(stack)) {
+			if (level.isClientSide()) {
+				player.displayClientMessage(TextComponentUtil.getFailureMsgText("not_enough_nutrients"), true);
+				player.playSound(ModSoundEvents.FLESHKIN_NO.get(), 0.8f, 0.8f + player.level().getRandom().nextFloat() * 0.4f);
+			}
+			return InteractionResultHolder.fail(stack);
+		}
+
+		return super.use(level, player, hand);
+	}
+
+	@Override
+	public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
+		if (level.isClientSide()) return;
+
+		if (!hasNutrients(stack)) {
+			livingEntity.stopUsingItem();
+		}
+	}
+
+	public void damageCurrentlyUsedLivingShield(ItemStack usedShield, float damageAmount, LivingEntity entity) {
+		if (!MobUtil.isCreativePlayer(entity)) {
+			int amount = 1 + Mth.floor(damageAmount);
+			decreaseNutrients(usedShield, amount);
+		}
+	}
+
+	@Override
+	public boolean isBarVisible(ItemStack stack) {
+		return getNutrients(stack) > 0;
+	}
+
+	@Override
+	public int getBarWidth(ItemStack stack) {
+		return Math.round(getNutrientsPct(stack) * 13f);
+	}
+
+	@Override
+	public int getBarColor(ItemStack stack) {
+		return ColorStyles.NUTRIENTS_FUEL_BAR;
+	}
+
+	@Override
+	public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+		return slotChanged;
+	}
+
+	protected void playSound(Player player, SoundEvent soundEvent) {
+		player.playSound(soundEvent, 0.8f, 0.8f + player.level().getRandom().nextFloat() * 0.4f);
+	}
+
+	@Override
+	public boolean isDamageable(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public void setDamage(ItemStack stack, int damage) {
+		//do nothing
+	}
+
+	@Override
+	public int getDamage(ItemStack stack) {
+		return 0;
+	}
+
+	@Override
+	public int getMaxDamage(ItemStack stack) {
+		return 0;
+	}
+
+	@Override
+	public boolean canBeDepleted() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnchantable(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+		return false;
+	}
+
+}
--- src/main/java/com/github/elenterius/biomancy/item/shield/ThornShieldItem.java
+++ src/main/java/com/github/elenterius/biomancy/item/shield/ThornShieldItem.java
@@ -1,8 +1,8 @@
 package com.github.elenterius.biomancy.item.shield;
 
 import com.github.elenterius.biomancy.client.render.item.shield.ThornShieldRenderer;
+import com.github.elenterius.biomancy.init.ModSoundEvents;
 import com.github.elenterius.biomancy.item.ShieldBlockingListener;
-import com.github.elenterius.biomancy.item.SimpleItem;
 import com.github.elenterius.biomancy.styles.TextComponentUtil;
 import com.github.elenterius.biomancy.styles.TextStyles;
 import com.github.elenterius.biomancy.util.ComponentUtil;
@@ -11,15 +11,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Equipable;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-import net.minecraftforge.common.ToolAction;
-import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.constant.DataTickets;
@@ -32,14 +31,13 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class ThornShieldItem extends SimpleItem implements Equipable, ShieldBlockingListener, GeoItem {
+public class ThornShieldItem extends LivingShieldItem implements Equipable, ShieldBlockingListener, GeoItem {
 
-	public static final String BLOCKING_KEY = "blocking";
+	public static final String BLOCKING_TAG_KEY = "blocking";
 	private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-	public ThornShieldItem(Properties properties) {
-		super(properties);
-		DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
+	public ThornShieldItem(int maxNutrients, Properties properties) {
+		super(maxNutrients, properties);
 	}
 
 	@Override
@@ -51,45 +49,50 @@ public class ThornShieldItem extends SimpleItem implements Equipable, ShieldBloc
 	}
 
 	@Override
-	public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
-		return ToolActions.DEFAULT_SHIELD_ACTIONS.contains(toolAction);
-	}
-
-	@Override
 	public void onShieldBlocking(ItemStack shield, LivingEntity user, LivingEntity attacker) {
 		attacker.hurt(user.damageSources().thorns(user), 1.5f + user.getRandom().nextInt(4));
 	}
 
 	@Override
-	public EquipmentSlot getEquipmentSlot() {
-		return EquipmentSlot.OFFHAND;
-	}
-
-	@Override
-	public UseAnim getUseAnimation(ItemStack stack) {
-		return UseAnim.BLOCK;
-	}
-
-	@Override
-	public int getUseDuration(ItemStack stack) {
-		return 72000;
-	}
-
-	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack stack = player.getItemInHand(hand);
+
+		if (!hasNutrients(stack)) {
+			if (level.isClientSide()) {
+				player.displayClientMessage(TextComponentUtil.getFailureMsgText("not_enough_nutrients"), true);
+				player.playSound(ModSoundEvents.FLESHKIN_NO.get(), 0.8f, 0.8f + player.level().getRandom().nextFloat() * 0.4f);
+			}
+			return InteractionResultHolder.fail(stack);
+		}
+
 		player.startUsingItem(hand);
 		if (level instanceof ServerLevel serverLevel && player.isUsingItem() && player.getUseItem() == stack) {
 			GeoItem.getOrAssignId(stack, serverLevel);
-			stack.getOrCreateTag().putBoolean(BLOCKING_KEY, true);
+			stack.getOrCreateTag().putBoolean(BLOCKING_TAG_KEY, true);
 		}
 		return InteractionResultHolder.consume(stack);
 	}
 
 	@Override
 	public void onStopUsing(ItemStack stack, LivingEntity livingEntity, int timeUsed) {
-		if (!livingEntity.level().isClientSide()) {
-			stack.getOrCreateTag().putBoolean(BLOCKING_KEY, false);
+		if (livingEntity.level() instanceof ServerLevel serverLevel) {
+			GeoItem.getOrAssignId(stack, serverLevel);
+			stack.getOrCreateTag().putBoolean(BLOCKING_TAG_KEY, false);
+		}
+	}
+
+	@Override
+	public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean isSelected) {
+		if (!isSelected) return;
+		if (level.isClientSide) return;
+
+		//fallback, because in some rare cases the `IForgeItem.onStopUsing()` method isn't called
+		if (entity instanceof LivingEntity livingEntity && livingEntity.getUseItem() != stack) {
+			boolean isBlocking = stack.getOrCreateTag().getBoolean(BLOCKING_TAG_KEY);
+			if (isBlocking) {
+				GeoItem.getOrAssignId(stack, (ServerLevel) level);
+				stack.getOrCreateTag().putBoolean(BLOCKING_TAG_KEY, false);
+			}
 		}
 	}
 
@@ -109,19 +112,18 @@ public class ThornShieldItem extends SimpleItem implements Equipable, ShieldBloc
 	@Override
 	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
 		AnimationController<ThornShieldItem> controller = new AnimationController<>(this, "main", state -> {
+
 			ItemStack stack = state.getData(DataTickets.ITEMSTACK);
-			boolean isBlocking = stack.getOrCreateTag().getBoolean(BLOCKING_KEY);
+			boolean isBlocking = stack.getOrCreateTag().getBoolean(BLOCKING_TAG_KEY);
 
 			if (isBlocking) {
-				if (state.isCurrentAnimationStage("retracted")) {
-					return state.setAndContinue(Animations.TRANSITION_TO_EXTENDED);
-				}
-				return state.setAndContinue(Animations.EXTENDED);
+				return state.setAndContinue(Animations.TRANSITION_TO_EXTENDED);
 			}
 
-			if (state.isCurrentAnimationStage("extended")) {
+			if (!state.isCurrentAnimationStage("retracted")) {
 				return state.setAndContinue(Animations.TRANSITION_TO_RETRACTED);
 			}
+
 			return state.setAndContinue(Animations.RETRACTED);
 		});
 
@@ -134,10 +136,9 @@ public class ThornShieldItem extends SimpleItem implements Equipable, ShieldBloc
 	}
 
 	protected static final class Animations {
-		public static final RawAnimation TRANSITION_TO_RETRACTED = RawAnimation.begin().thenPlay("retract").thenPlay("retracted");
 		public static final RawAnimation RETRACTED = RawAnimation.begin().thenPlay("retracted");
+		public static final RawAnimation TRANSITION_TO_RETRACTED = RawAnimation.begin().thenPlay("retract").thenPlay("retracted");
 		public static final RawAnimation TRANSITION_TO_EXTENDED = RawAnimation.begin().thenPlay("extend").thenPlay("extended");
-		public static final RawAnimation EXTENDED = RawAnimation.begin().thenPlay("extended");
 
 		private Animations() {}
 
