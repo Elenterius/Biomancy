d246f39da64cc46a5bd8743410609fe00071aa9f
Elenterius <w.elenterius@gmail.com>
2024-07-18 23:55:34 +0200

    fix: fix Acolyte Armor z-fighting and render type

--- src/main/java/com/github/elenterius/biomancy/client/render/item/armor/AcolyteArmorRenderer.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/item/armor/AcolyteArmorRenderer.java
@@ -2,10 +2,6 @@ package com.github.elenterius.biomancy.client.render.item.armor;
 
 import com.github.elenterius.biomancy.BiomancyMod;
 import com.github.elenterius.biomancy.item.armor.AcolyteArmorItem;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
@@ -13,11 +9,6 @@ public final class AcolyteArmorRenderer extends GeoArmorRenderer<AcolyteArmorIte
 
 	public AcolyteArmorRenderer() {
 		super(new DefaultedItemGeoModel<>(BiomancyMod.createRL("armor/acolyte_armor")));
-	}
-
-	@Override
-	public RenderType getRenderType(AcolyteArmorItem animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
-		return RenderType.entityCutout(texture);
 	}
 
 }
