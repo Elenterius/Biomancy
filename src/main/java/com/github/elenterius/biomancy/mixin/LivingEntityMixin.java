f9d612bcdb523c35c82477556d867f65ff0944f6
Elenterius <w.elenterius@gmail.com>
2024-08-31 02:39:31 +0200

    feat: add rare chance to get Primordial Infection when eating raw meat

--- src/main/java/com/github/elenterius/biomancy/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/elenterius/biomancy/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package com.github.elenterius.biomancy.mixin;
 
 import com.github.elenterius.biomancy.init.ModMobEffects;
+import com.github.elenterius.biomancy.init.tags.ModItemTags;
 import com.github.elenterius.biomancy.item.ShieldBlockingListener;
 import com.github.elenterius.biomancy.serum.FrenzySerum;
 import com.github.elenterius.biomancy.statuseffect.StatusEffectHandler;
@@ -14,7 +15,9 @@ import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -65,6 +68,20 @@ public abstract class LivingEntityMixin {
 				cir.setReturnValue(false);
 			}
 		}
+	}
+
+	@Inject(method = "addEatEffect", at = @At(value = "TAIL"))
+	private void onAddEatEffect(ItemStack food, Level level, LivingEntity livingEntity, CallbackInfo ci) {
+		if (!level.isClientSide && biomancy$getRawMeatNutrition(food) > 2 && livingEntity.getRandom().nextFloat() < 0.2f) {
+			livingEntity.addEffect(new MobEffectInstance(ModMobEffects.PRIMORDIAL_INFESTATION.get(), 20 * 8, 0));
+		}
+	}
+
+	@Unique
+	private static int biomancy$getRawMeatNutrition(ItemStack itemStack) {
+		if (!itemStack.isEdible()) return 0;
+		FoodProperties food = itemStack.getFoodProperties(null);
+		return food != null && food.isMeat() && itemStack.is(ModItemTags.RAW_MEATS) ? food.getNutrition() : 0;
 	}
 
 	@Unique
