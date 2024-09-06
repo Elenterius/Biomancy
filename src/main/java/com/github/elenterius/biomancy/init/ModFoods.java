c82fb7764a71a3bab5d40d72324675c18baa882a
Elenterius <w.elenterius@gmail.com>
2024-09-07 01:18:06 +0200

    feat: nerf Nutrient Bar saturation from 1.8 to 1.2 and buff Nutrient Paste saturation from 0.2 to 0.6

--- src/main/java/com/github/elenterius/biomancy/init/ModFoods.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModFoods.java
@@ -21,16 +21,14 @@ public final class ModFoods {
 	public static final FoodProperties MARROW_FLUID = new FoodProperties.Builder().nutrition(1).saturationMod(0.2f).build();
 
 	public static final FoodProperties POOR_FLESH = new FoodProperties.Builder().nutrition(1).saturationMod(0.2f).meat().build();
-	public static final FoodProperties AVERAGE_FLESH = new FoodProperties.Builder().nutrition(2).saturationMod(0.4f).meat().build();
 
 	public static final FoodProperties LIVING_FLESH = new FoodProperties.Builder().nutrition(2).saturationMod(0.4f).meat()
 			.effect(() -> new MobEffectInstance(ModMobEffects.PRIMORDIAL_INFESTATION.get(), 20 * 20, 0), 0.4f)
 			.effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 20 * 6, 0), 0.8f)
 			.build();
 
-	public static final FoodProperties NUTRIENT_BAR = new FoodProperties.Builder().nutrition(9).saturationMod(1.8f).build();
-	public static final FoodProperties NUTRIENT_PASTE = new FoodProperties.Builder().nutrition(1).saturationMod(0.2f).build();
-	public static final FoodProperties PROTEIN_BAR = new FoodProperties.Builder().nutrition(5).saturationMod(0.4f).meat().build();
+	public static final FoodProperties NUTRIENT_BAR = new FoodProperties.Builder().nutrition(9).saturationMod(1.2f).build();
+	public static final FoodProperties NUTRIENT_PASTE = new FoodProperties.Builder().nutrition(1).saturationMod(0.6f).build();
 
 	private ModFoods() {}
 
