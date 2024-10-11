7a472624d04388fe837a46cbf82ed9ea5ea99abb
Elenterius <w.elenterius@gmail.com>
2024-10-11 21:33:28 +0200

    chore: refactor in world acid digestion and make it recipe result agnostic

--- src/main/java/com/github/elenterius/biomancy/mixin/ItemEntityMixin.java
+++ src/main/java/com/github/elenterius/biomancy/mixin/ItemEntityMixin.java
@@ -9,10 +9,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
-	@Inject(at=@At("TAIL"),method={"tick()V"})
+
+	@Inject(at = @At("TAIL"), method = {"tick()V"})
 	private void onTick(CallbackInfo ci) {
-		ItemEntity itemEntity = (ItemEntity)((Object)this); //I hate casting like this on so many levels -Kd
+		ItemEntity itemEntity = (ItemEntity) (Object) this; //I hate casting like this on so many levels -Kd
 		if (itemEntity.isRemoved()) return;
 		AcidInteractions.InWorldItemDigesting.tryDigestSubmergedItem(itemEntity);
 	}
+
 }
