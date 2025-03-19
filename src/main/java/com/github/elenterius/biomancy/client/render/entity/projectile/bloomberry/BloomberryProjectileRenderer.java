ec02c71a74df0870747570d3bff3e65505f118d6
Elenterius <w.elenterius@gmail.com>
2025-03-19 12:20:36 +0100

    fix: fix acid and bloomberry projectile models not aligning with movement direction

--- src/main/java/com/github/elenterius/biomancy/client/render/entity/projectile/bloomberry/BloomberryProjectileRenderer.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/entity/projectile/bloomberry/BloomberryProjectileRenderer.java
@@ -1,7 +1,14 @@
 package com.github.elenterius.biomancy.client.render.entity.projectile.bloomberry;
 
 import com.github.elenterius.biomancy.entity.projectile.BloomberryProjectile;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class BloomberryProjectileRenderer extends GeoEntityRenderer<BloomberryProjectile> {
@@ -10,8 +17,13 @@ public class BloomberryProjectileRenderer extends GeoEntityRenderer<BloomberryPr
 		super(renderManager, new BloomberryModel());
 	}
 
-	//poseStack.translate(0, projectile.getBbHeight() / 2f, 0);
-	//poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTick, projectile.yRotO, projectile.getYRot()) + 180f)); //wtf?
-	//poseStack.mulPose(Axis.ZP.rotationDegrees(Mth.lerp(partialTick, projectile.xRotO, projectile.getXRot())));
+	@Override
+	public void actuallyRender(PoseStack poseStack, BloomberryProjectile animatable, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+
+		poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot())));
+		poseStack.mulPose(Axis.XP.rotationDegrees(-Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot())));
+
+		super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+	}
 
 }
