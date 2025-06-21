ccc45d56e59c6cb216c11e3bf1db2797b1c9a19a
Elenterius <w.elenterius@gmail.com>
2025-06-21 20:06:46 +0200

    fix: prevent watergel from being broken by flowing water

--- src/main/java/com/github/elenterius/biomancy/init/ModBlocks.java
+++ src/main/java/com/github/elenterius/biomancy/init/ModBlocks.java
@@ -134,7 +134,7 @@ public final class ModBlocks {
 	public static final RegistryObject<LiquidBlock> ACID_FLUID_BLOCK = register("acid_fluid_block", () -> new LiquidBlock(ModFluids.ACID, copyProperties(Blocks.WATER)));
 	public static final RegistryObject<AcidCauldron> ACID_CAULDRON = register("acid_cauldron", () -> new AcidCauldron(copyProperties(Blocks.CAULDRON)));
 	public static final RegistryObject<AcidSplatterBlock> ACID_SPLATTER = register("acid_splatter", () -> new AcidSplatterBlock(createProperties().mapColor(MapColor.COLOR_LIGHT_GREEN).noOcclusion().noCollission().instabreak().replaceable().pushReaction(PushReaction.DESTROY).sound(SoundType.FROGSPAWN)));
-	public static final RegistryObject<WaterGelBlock> WATER_GEL_BLOCK = register("water_gel_block", properties -> new WaterGelBlock(properties.mapColor(MapColor.WATER).strength(100f).speedFactor(0.8f).jumpFactor(1.2f).noOcclusion().sound(SoundType.SLIME_BLOCK).noOcclusion().isRedstoneConductor(ModBlocks::neverValid)));
+	public static final RegistryObject<WaterGelBlock> WATER_GEL_BLOCK = register("water_gel_block", properties -> new WaterGelBlock(properties.mapColor(MapColor.WATER).strength(100f).speedFactor(0.8f).jumpFactor(1.2f).noOcclusion().forceSolidOn().sound(SoundType.SLIME_BLOCK).isRedstoneConductor(ModBlocks::neverValid)));
 
 	//## Misc
 	public static final RegistryObject<LadderBlock> FLESH_LADDER = register("flesh_ladder", () -> new LadderBlock(createBonyFleshProperties().noOcclusion()));
